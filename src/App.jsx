19922bf8891bb53200304730fb4e6217a0614752
k19weandweb <k19weandweb@gmai.com>
2023-06-23 07:31:51 +0700

    fixx

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,12 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 
-// import PrivateRoute from "./routes/PrivateRoute";
+import PrivateRoute from "./routes/PrivateRoute";
 
 import "./App.css";
 import config from "./config";
 // import { MyThemeContextProvider } from "./contexts/ThemeContext";
-import {
-  //privateRoutes,
-  publicRoutes
-} from "./routes";
+import { privateRoutes, publicRoutes } from "./routes";
 import "./styles/GlobalStyles/GlobalStyles.scss";
 
 ReactGA.initialize(config.TRACKING_ID);
@@ -42,20 +39,20 @@ const App = () => {
                   />
                 );
               })}
-              {/* {privateRoutes.map((route) => {
-                  const Page = route.component;
-                  return (
-                    <Route
-                      key={route.path}
-                      path={route.path}
-                      element={
-                        <PrivateRoute>
-                          <Page />
-                        </PrivateRoute>
-                      }
-                    />
-                  );
-                })} */}
+              {privateRoutes.map((route) => {
+                const Page = route.component;
+                return (
+                  <Route
+                    key={route.path}
+                    path={route.path}
+                    element={
+                      <PrivateRoute>
+                        <Page />
+                      </PrivateRoute>
+                    }
+                  />
+                );
+              })}
             </Routes>
           </Router>
         </div>
